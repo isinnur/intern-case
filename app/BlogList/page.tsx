683aa5ed850091a2c064994dcf1de5e58d0aae3b
Işınnur Günay <isinngnyy@gmail.com>
2024-12-18 20:01:24 +0300

    feat: add bloglist

--- app/BlogList/page.tsx
+++ app/BlogList/page.tsx
@@ -1,31 +1,61 @@
 import Image from "next/image";
+import Card from "../components/Card";
 
 const BlogList = () => {
   return (
     <div className="w-340 mt-12 flex items-center justify-center flex-col">
-      <h2 className="font-semibold text-2xl ">Page Title</h2>
-      <div className="flex text-spanText gap-5 text-sm mb-12">
+      <h2 className="font-semibold text-3xl ">Page Title</h2>
+      <div className="flex text-spanText gap-4 text-sm mb-12 mt-2 justify-between">
         <span>Home</span>
+        <span>|</span>
         <span>Link One</span>
       </div>
-      {/* imageli olan kısım */}
-      <div>
-        {/* content */}
-        <div>
-          <Image
-            src="/assets/heroImg.png"
-            width="1216"
-            height="450"
-            alt="Hero image"
-          />
-          <span>Technology</span>
-          <span>
+
+      <div className="relative">
+        <Image
+          src="/assets/heroImg.png"
+          width="1216"
+          height="450"
+          alt="Hero image"
+        />
+        <div className="flex flex-col absolute bottom-10 left-10 text-white gap-4">
+          <span className="bg-blue-600 text-white p-3 rounded-md w-24 h-7 flex items-center justify-center font-medium">
+            Technology
+          </span>
+
+          <span className="text-3xl ">
             The Impact of Technology on the Workplace: How Technology is
             Changing
           </span>
-          <span>Tracey Wilson</span>
-          <span>August 20, 2022</span>
+          <div className="flex items-center gap-5 ">
+            <span className="flex items-center justify-center gap-3">
+              <Image
+                src="/assets/profile.png"
+                width={36}
+                height={36}
+                alt="Person Image"
+              />
+              Tracey Wilson
+            </span>
+            <span>August 20, 2022</span>
+          </div>
         </div>
+      </div>
+      <div className="my-12 grid grid-cols-3 gap-5 ">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      <div className=" mb-52">
+        <button className="border border-spacing-1 border-[#E8E8EA] py-3 px-5 rounded-md text-spanText">
+          Load More
+        </button>
       </div>
     </div>
   );
---
+++ app/components/Card.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+
+const Card = () => {
+  return (
+    <div className="w-[392px] h-[488px] rounded-xl border border-spacing-1 border-[#E8E8EA] flex flex-col items-center ">
+      <Image
+        src="/assets/blog1.png"
+        width={360}
+        height={240}
+        alt="Blog image "
+        className="rounded-md mt-4"
+      />
+
+      <div className="w-[344px]">
+        <div className="mt-6 ">
+          <span className="bg-blue-50 text-blue-700 p-1 mb-2 w-fit rounded-md">
+            Technology
+          </span>
+          <h2 className="text-xl font-semibold mt-4">
+            The Impact of Technology on the Workplace: How Technology is
+            Changing
+          </h2>
+        </div>
+
+        <div className="mt-5 flex items-center gap-5 ">
+          <span className="flex items-center gap-3">
+            <Image
+              src="/assets/blogPerson1.png"
+              width={36}
+              height={36}
+              alt="Person Image"
+            />
+            Tracey Wilson
+          </span>
+          <span>August 20, 2022</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
