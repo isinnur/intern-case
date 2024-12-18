c22da776d1b688d472e6776abd7d7901b24e79ee
Işınnur Günay <isinngnyy@gmail.com>
2024-12-19 00:41:28 +0300

    refactor(navbar): update icon placement

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = () => {
 
       {/* Linkler burda olcak */}
 
-      <ul className="flex  items-center justify-between gap-10 text-lightText ">
+      <ul className="flex items-center justify-between gap-10 text-lightText ">
         <li className="cursor-pointer">Home</li>
         <li className="cursor-pointer">Blog</li>
         <li className="cursor-pointer">Single Post</li>
@@ -24,16 +24,25 @@ const Navbar = () => {
       </ul>
 
       {/* arama / theme */}
-      <div className=" flex gap-10 w-300">
-        <div className="flex">
+
+      <div className="flex gap-10 items-center justify-center">
+        <div className="flex border bg-lightButton rounded-md items-center justify-between px-2">
           <input
             type="text"
-            className="bg-lightButton p-3 pr-6 rounded-md w-full outline-none"
+            className="outline-none bg-lightButton h-9"
             placeholder="Search"
           />
-          <IoIosSearch className="text-lightText" />
+          <IoIosSearch />
         </div>
-        <div className="w-20 h-10 bg-lightButton rounded-full"></div>
+        <div className="w-12 h-7 bg-lightButton rounded-full flex items-center">
+          <Image
+            src="/assets/light.png"
+            width={24}
+            height={24}
+            alt="light Image Icon"
+            className="cursor-pointer"
+          />
+        </div>
       </div>
     </div>
   );
